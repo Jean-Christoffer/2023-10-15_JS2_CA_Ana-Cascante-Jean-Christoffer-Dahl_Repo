7704850ad43c9cc77657654836ad6b4a46d3a001
Jean Christoffer <89041011+Jean-Christoffer@users.noreply.github.com>
2023-09-14 19:03:44 +0200

    form styling and function utils -folder
    Moved loginFunction to helpers folder
    noroff users can now login
    Error helpers shows up if conditions are not met

--- html/login.js
+++ html/login.js
@@ -1,28 +1,47 @@
 
+import logIn  from "./utils/helpers/loginFunc.js";
 const selectors =  [
     "#email",
     "#password",
-    "#login_form"
+    "#login_form",
+    "#passwordError",
+    "#emailError"
 ]
+
 const selected = selectors.map(value => document.querySelector(value));
 const mailRegex = /^[a-zA-Z0-9._%+-]+@(stud\.)?noroff\.no$/;
 const baseURL = "https://api.noroff.dev/api/v1"
 const [
     email,
     password,
-    loginForm
+    loginForm,
+    passwordError,
+    emailError
 ] = selected;
 
 
 loginForm.addEventListener("submit", async(e) => {
     e.preventDefault();
     const emailValue = email.value.trim().toLowerCase();
-    const passwordValue = password
+    const passwordValue = password.value
   
-    
-    if(passwordValue.value.length >= 8 &&  mailRegex.test(emailValue)){
+    let isFormValid = true;
+    passwordError.textContent = '';
+    emailError.textContent = '';
+
+    if (passwordValue.length < 8) {
+        passwordError.textContent = 'Password must be at least 8 characters long.';
+        isFormValid = false;
+    }
+
+    if (!mailRegex.test(emailValue)) {
+        emailError.textContent = 'Invalid email format.';
+        isFormValid = false;
+    }
+
+    if(isFormValid){
         try{
-            logIn(`${baseURL}`,{"email":`${emailValue}`,"password":`${passwordValue.value}`})
+            logIn(`${baseURL}`,{"email":`${emailValue}`,"password":`${passwordValue}`})
         }catch(err){
             console.log(err)
         }
@@ -32,41 +51,3 @@ loginForm.addEventListener("submit", async(e) => {
 })
 
 
-
-//login func
-async function logIn(url, data={}){
-
-    try{
-        const response = await fetch(`${url}/social/auth/login`,{
-            method:"POST",
-            credentials:"same-origin",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify(data)
-    })
-    if (!response.ok) {
-        console.log(response)
-        throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const responseData = await response.json()
-    console.log(responseData)
-    const token = responseData.accessToken
-    if (token) {
-        localStorage.setItem('bearerToken', token);
-        if(responseData && token){
-            //window.location.href = "profile.html";
-            
-            console.log("succ")
-        }
-    } else {
-        console.error("Token not found in response");
-    }
-    
-    return responseData;
-    }catch(err){
-        console.log(err)
-    }
-
-
-}
