962cee5e3cf036b22064d80c15bef8bc9d9dc241
Jean Christoffer <89041011+Jean-Christoffer@users.noreply.github.com>
2023-09-17 16:23:03 +0200

    register

--- html/register/register.js
+++ html/register/register.js
@@ -49,5 +49,43 @@ loginForm.addEventListener("submit", async(e) => {
   
     }
 })
+//register func
+async function logIn(url, data={}){
 
+    try{
+        const response = await fetch(`${url}/social/auth/login`,{
+            method:"POST",
+            credentials:"same-origin",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(data)
+    })
+    if (!response.ok) {
+     
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const responseData = await response.json()
+
+    const token = responseData.accessToken
+    if (token) {
+        localStorage.setItem('bearerToken', token);
+        if(responseData && token){
+            window.location.href = "profile.html";
+            
+            console.log("succ")
+        }
+    } else {
+        console.error("Token not found in response");
+    }
+    
+    return responseData;
+    }catch(err){
+        console.log(err)
+    }
+
+
+}
+
+export default logIn
 
